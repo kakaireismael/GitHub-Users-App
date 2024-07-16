696a9be8326b8cc4321d3a5bd57fdff0c1ec7e97
kakaireismael <kakaireismael86@gmail.com>
2024-07-16 18:13:59 +0300

    feat(UI):make changes to the UI -improve UI -Add splash screen

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -85,7 +85,7 @@ class _HomePageState  extends State<HomePage>{
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(50.0)
                     ),
-                    hintText: 'Enter Country',
+                    hintText: 'Search by Country',
                     hintStyle: const TextStyle(color: Colors.grey),
                     suffixIcon: IconButton( onPressed: (){
                       _searchUsers(_controller.text);
@@ -156,4 +156,112 @@ class _HomePageState  extends State<HomePage>{
     );
   }
 
+}
+class InfiniteScrollExample extends StatefulWidget {
+  const InfiniteScrollExample({super.key});
+
+  @override
+  State<InfiniteScrollExample> createState() => _InfiniteScrollExampleState();
+}
+
+class _InfiniteScrollExampleState extends State<InfiniteScrollExample> {
+  final int _pageSize = 20;
+
+  final PagingController<int, dynamic> _pagingController =
+  PagingController(firstPageKey: 1);
+
+  @override
+  void initState() {
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchPage(pageKey);
+    });
+    super.initState();
+  }
+
+  Future<void> _fetchPage(int pageKey) async {
+    try {
+      // get api /beers list from pages
+      final newItems = await RemoteApi.getBeerList(pageKey, _pageSize);
+      // Check if it is last page
+      final isLastPage = newItems!.length < _pageSize;
+      // If it is last page then append last page else append new page
+      if (isLastPage) {
+        _pagingController.appendLastPage(newItems);
+      } else {
+        // Appending new page when it is not last page
+        final nextPageKey = pageKey + 1;
+        _pagingController.appendPage(newItems, nextPageKey);
+      }
+    }
+    // Handle error in catch
+    catch (error) {
+      print(_pagingController.error);
+      // Sets the error in controller
+      _pagingController.error = error;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      // Refrsh Indicator pull down
+  RefreshIndicator(
+    onRefresh: () => Future.sync(
+      // Refresh through page controllers
+          () => _pagingController.refresh(),
+    ),
+    child: Scaffold(
+      appBar: AppBar(
+        title: const Text("Pagination Scroll Flutter Template"),
+      ),
+      // Page Listview with divider as a separation
+      body: PagedListView<int, dynamic>.separated(
+        pagingController: _pagingController,
+        builderDelegate: PagedChildBuilderDelegate<dynamic>(
+          animateTransitions: true,
+          itemBuilder: (_, item, index) => ListTile(
+            leading: CircleAvatar(
+              radius: 20,
+              backgroundImage: NetworkImage(item["image_url"]),
+            ),
+            title: Text(item["name"]),
+          ),
+        ),
+        separatorBuilder: (_, index) => const Divider(),
+      ),
+    ),
+  );
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
+  }
+}
+
+class RemoteApi {
+  static Future<List<dynamic>?> getBeerList(
+      // Page means on which page you are currently
+      int page,
+      // Limit per page you want to set
+      int limit,
+      ) async {
+    try {
+      // Request the API on url
+      final response = await http.get(
+        Uri.parse(
+          'https://api.punkapi.com/v2/beers?'
+              'page=$page'
+              '&per_page=$limit',
+        ),
+      );
+      if (response.statusCode == 200) {
+        // Decode the response
+        final mybody = jsonDecode(response.body);
+        return mybody;
+      }
+    } catch (e) {
+      print("Error $e");
+    }
+    return null;
+  }
 }
