09402b03aae230ab6bc64bb1027b729780ce97bb
kakaireismael <kakaireismael86@gmail.com>
2024-08-17 21:07:04 +0300

    Add dependecy injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:github_users_app/data/repository/user_profile_rep_imp.dart';
-import 'package:github_users_app/domain/use_cases/user_details_useCase.dart';
-import '/data/dataSources/remote/data_source.dart';
-import '/domain/use_cases/get_users_useCase.dart';
 import '/presentation/providers/user_provider.dart';
-import '/data/repository/user_repository_impl.dart';
 import '/presentation/providers/user_profile_provider.dart';
 import '/presentation/providers/connectivity_provider.dart';
 import '/presentation/screens/splash_screen.dart';
-import '/data/datasources/remote/details_source.dart';
-import '/presentation/providers/services/api_service.dart';
 import 'package:github_users_app/core/di/service_locator.dart';  // Import the service locator
-import 'package:url_launcher/url_launcher.dart';
-
 
 void main() {
   setupLocator();  // Initialize get_it
   runApp(const MyApp());
 }
-
-
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
--- lib/presentation/widget/connectivity.dart
+++ lib/presentation/widget/connectivity.dart
@@ -14,98 +14,101 @@ Widget connectivityWidget(BuildContext context) {
       ),
       iconTheme: const IconThemeData(color: Colors.white),
     ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(height: 20),
-          Image.asset(
-            'assets/no_internet.png', // Replace with your image asset path
-            width: 200, // Adjust the width as needed
-            height: 200, // Adjust the height as needed
-            fit: BoxFit.contain, // Adjust the fit as needed
-          ),
-          const SizedBox(height: 20), // Space between image and text
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40.0),
-            child: Text(
-              'Oops! No Internet Connection!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.black87,
-                fontSize: 23.0,
-                fontWeight: FontWeight.bold,
+    body: Container(
+      color: Colors.white, // Set background color for the whole screen
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            Image.asset(
+              'assets/no_internet.gif', // Ensure this path is correct
+              width: 250, // Adjust the width as needed
+              height: 250, // Adjust the height as needed
+              fit: BoxFit.contain, // Adjust the fit as needed
+            ),
+            const SizedBox(height: 20), // Space between image and text
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+              child: Text(
+                'Oops! No Internet Connection!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 23.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 50.0),
-            child: Text(
-              'It seems there is something wrong with your internet connection. Please connect to the internet and try again.',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.black54,
-                fontSize: 16.0,
-                fontWeight: FontWeight.normal,
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 50.0),
+              child: Text(
+                'It seems there is something wrong with your internet connection. Please connect to the internet and try again.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.normal,
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 20), // Space between text and button
-          ElevatedButton(
-            onPressed: () {
-              Connectivity().checkConnectivity().then((result) {
-                if (result == ConnectivityResult.none) {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text(
-                          "Check your network cables, modem, routers, and Wi-Fi connection",
-                        ),
-                        content: const Text(
-                          "Please check your connection and try again.",
-                        ),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: const Text(
-                              "Cancel",
-                              style: TextStyle(color: Colors.black54),
-                            ),
+            const SizedBox(height: 20), // Space between text and button
+            ElevatedButton(
+              onPressed: () {
+                Connectivity().checkConnectivity().then((result) {
+                  if (result == ConnectivityResult.none) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text(
+                            "Check your network cables, modem, routers, and Wi-Fi connection",
                           ),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                              openSettings();
-                            },
-                            child: const Text(
-                              "Internet Settings",
-                              style: TextStyle(color: Colors.black54),
-                            ),
+                          content: const Text(
+                            "Please check your connection and try again.",
                           ),
-                        ],
-                      );
-                    },
-                  );
-                }
-              });
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue, // Button background color
-            ),
-            child: const Text(
-              'Try Again',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text(
+                                "Cancel",
+                                style: TextStyle(color: Colors.black54),
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                                openSettings();
+                              },
+                              child: const Text(
+                                "Internet Settings",
+                                style: TextStyle(color: Colors.black54),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                });
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue, // Button background color
+              ),
+              child: const Text(
+                'Try Again',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     ),
   );
